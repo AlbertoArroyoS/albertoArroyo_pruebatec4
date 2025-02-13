d424e7e14ea3c7b667407dc5c0b5c9208bf667ae
Alberto Arroyo <albertoarroyo@hotmail.es>
2025-02-13 12:30:37 +0100

    Add SecurityConfig to configure security settings and allow all HTTP methods

---
+++ travelagency/src/main/java/com/hackaboss/travelagency/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.hackaboss.travelagency.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+
+    // Configuración de seguridad para permitir todas las solicitudes
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                // Deshabilita la protección CSRF
+                .csrf(csrf -> csrf.disable())
+
+                // Configura las reglas de autorización de solicitudes
+                .authorizeHttpRequests(authRequest ->
+                        authRequest
+                                //Permitir solicitures get y options
+                                .requestMatchers(HttpMethod.GET).permitAll()
+                                .requestMatchers(HttpMethod.OPTIONS).permitAll()
+                                .requestMatchers(HttpMethod.POST).permitAll()
+                                .requestMatchers(HttpMethod.PUT).permitAll()
+                                .requestMatchers(HttpMethod.DELETE).permitAll())
+
+                // Construye la cadena de filtros de seguridad
+                .build();
+    }
+
+}
