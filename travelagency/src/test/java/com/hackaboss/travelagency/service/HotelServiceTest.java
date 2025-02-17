5a7e2e0a4049fd3d2eb306c447bb022619597aaa
Alberto Arroyo <albertoarroyo@hotmail.es>
2025-02-17 18:34:49 +0100

    Update SQL data and Postman collection; add new hotel and flight entries, and enhance user data

--- travelagency/src/test/java/com/hackaboss/travelagency/service/HotelServiceTest.java
+++ travelagency/src/test/java/com/hackaboss/travelagency/service/HotelServiceTest.java
@@ -1,25 +1,44 @@
 package com.hackaboss.travelagency.service;
 
-import com.hackaboss.travelagency.dto.request.HotelDTORequest;
-import com.hackaboss.travelagency.exception.InvalidDataException;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
 
 import java.time.LocalDate;
 
-@SpringBootTest
+import com.hackaboss.travelagency.dto.request.HotelDTORequest;
+import com.hackaboss.travelagency.exception.InvalidDataException;
+import com.hackaboss.travelagency.mapper.HotelMapper;
+import com.hackaboss.travelagency.model.Hotel;
+import com.hackaboss.travelagency.repository.HotelRepository;
+import com.hackaboss.travelagency.util.RoomType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.doReturn;
+
+
+@ExtendWith(MockitoExtension.class)
 class HotelServiceTest {
 
-    @Autowired
-    private HotelService hotelService;
+    @InjectMocks
+    private HotelService hotelService; // Servicio a probar
+
+    @Mock
+    private HotelRepository hotelRepository; // Mock para el repositorio
+
+    @Mock
+    private HotelMapper hotelMapper; // Mock para el mapper
 
     @Test
     void createHotelSuccessTest() {
         // Dado un HotelDTORequest válido
         HotelDTORequest dto = new HotelDTORequest();
-        dto.setHotelCode("HTL-54321");
+        dto.setHotelCode("HTL-59999");
         dto.setName("Hotel Sunrise");
         dto.setCity("Barcelona");
         dto.setRoomType("SUITE");
@@ -27,11 +46,27 @@ class HotelServiceTest {
         dto.setDateFrom(LocalDate.of(2024, 7, 1));
         dto.setDateTo(LocalDate.of(2024, 7, 20));
 
+        // Creamos la entidad Hotel que se espera recibir
+        Hotel hotelEntity = new Hotel();
+        hotelEntity.setHotelCode(dto.getHotelCode());
+        hotelEntity.setName(dto.getName());
+        hotelEntity.setCity(dto.getCity());
+        hotelEntity.setRoomType(RoomType.valueOf(dto.getRoomType()));
+        hotelEntity.setRatePerNight(dto.getRatePerNight());
+        hotelEntity.setDateFrom(dto.getDateFrom());
+        hotelEntity.setDateTo(dto.getDateTo());
+
+        // Configuramos el comportamiento de los mocks usando doReturn()
+        doReturn(hotelEntity).when(hotelMapper).requestToEntity(dto);
+        doReturn(hotelEntity).when(hotelRepository).save(any(Hotel.class));
+
         // Cuando se crea el hotel
         String response = hotelService.createHotel(dto);
 
         // Entonces se debe recibir el mensaje de éxito
         Assertions.assertEquals("Hotel creado con éxito", response);
+        verify(hotelMapper, times(1)).requestToEntity(dto);
+        verify(hotelRepository, times(1)).save(any(Hotel.class));
     }
 
     @Test
@@ -42,4 +77,3 @@ class HotelServiceTest {
         });
     }
 }
-
